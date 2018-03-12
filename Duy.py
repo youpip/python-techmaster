3af4dcd619bed21c5c796d377e9aeafd2c350e4e
youpip <37202369+youpip@users.noreply.github.com>
2018-03-12 05:13:21 +0100

    Add files via upload

---
+++ Duy.py
@@ -0,0 +1,24 @@
+score = 40
+
+if score >= 90:
+    print ("grade is A")
+elif score >= 80:
+    print ("grade is B")
+elif score >= 70:
+    print ("grade is C")
+elif score >= 60:
+    print ("grade is D")
+else:
+    print ("grade is E")
+
+BMI = 35
+
+if BMI < 18.5:
+    print ("Underweight")
+elif BMI <= 25:
+    print ("Normal")
+elif BMI <= 30:
+    print ("Overweight")
+else:
+    print ("Obese")
+
